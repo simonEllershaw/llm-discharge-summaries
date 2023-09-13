367c10229c344ee0ad559f6d983429eb362245c4
simonEllershaw <simje@live.co.uk>
2023-09-13 12:54:36 +0100

    Pydantic added

--- discharge_summaries/schemas/prsb_guidelines.py
+++ discharge_summaries/schemas/prsb_guidelines.py
@@ -1,17 +1,18 @@
-from typing import List
-
 from pydantic import BaseModel
 
 
 class Element(BaseModel):
     name: str
     description: str
-    cardinality: str
-    values: str
+
+
+class RecordEntry(BaseModel):
+    name: str
+    description: str
+    elements: list[Element]
 
 
 class Section(BaseModel):
     name: str
     description: str
-    is_record: bool
-    elements: List[Element]
+    elements: list[Element] | RecordEntry
