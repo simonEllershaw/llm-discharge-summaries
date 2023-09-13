6c2fcd7ae4b0ae87fc74a81a2fe8af4830f5d150
simonEllershaw <simje@live.co.uk>
2023-09-13 14:36:20 +0100

    Restrict to last note

--- discharge_summaries/schemas/prsb_guidelines.py
+++ discharge_summaries/schemas/prsb_guidelines.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 class Element(BaseModel):
     name: str
     description: str
+    values: str
+    snomed_codes: list[str] | None
 
 
 class RecordEntry(BaseModel):
@@ -16,3 +18,4 @@ class Section(BaseModel):
     name: str
     description: str
     elements: list[Element] | RecordEntry
+    restrict_to_last_note: bool
