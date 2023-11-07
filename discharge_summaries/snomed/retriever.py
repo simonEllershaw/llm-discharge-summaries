32fde38d4db43ff3d72c53cef3e12239df8a4217
simonEllershaw <simje@live.co.uk>
2023-11-07 14:04:26 +0000

    Update retriever to use token window

--- discharge_summaries/snomed/retriever.py
+++ discharge_summaries/snomed/retriever.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
-from typing import Dict, List, Set, Tuple
+from typing import Dict, List, Set
 
-from spacy.language import Language
 from spacy.tokens import Span
 
 from .lookup import SnomedLookup
@@ -11,69 +10,82 @@ from .phrase_matcher import SnomedPhraseMatcher
 class SnomedRetriever:
     def __init__(
         self,
-        base_snomed_phrase_matcher: SnomedPhraseMatcher,
+        snomed_phrase_matcher: SnomedPhraseMatcher,
         snomed_lookup: SnomedLookup,
-        nlp_with_sentence_segmenter: Language,
+        token_window_size: int = 50,
     ) -> None:
-        self._base_snomed_phrase_matcher = base_snomed_phrase_matcher
+        self._snomed_phrase_matcher = snomed_phrase_matcher
         self._snomed_lookup = snomed_lookup
-        self._nlp_with_sentence_segmenter = nlp_with_sentence_segmenter
+        self._token_window_size = token_window_size
 
-    def _create_custom_phrase_matcher(
-        self, search_term_to_cuis: Dict[str, List[int]]
-    ) -> SnomedPhraseMatcher:
-        custom_phrase_matcher = SnomedPhraseMatcher(
-            self._nlp_with_sentence_segmenter, False
-        )
-        for cui in {cui for cuis in search_term_to_cuis.values() for cui in cuis}:
-            custom_phrase_matcher.add_parent_cui(int(cui), self._snomed_lookup)
-
-        unmatched_finding_cui = -1
-        for finding, cuis in search_term_to_cuis.items():
-            if not cuis:
-                custom_phrase_matcher.add_non_snomed_term(
-                    unmatched_finding_cui, finding
-                )
-                search_term_to_cuis[finding] = [unmatched_finding_cui]
-                unmatched_finding_cui -= 1
-        return custom_phrase_matcher
-
-    def __call__(
-        self, texts: List[str], search_terms: Set[str]
-    ) -> Dict[str, List[Tuple[int, Span]]]:
+    def _get_cui_to_search_terms(self, search_terms: Set[str], include_child_cuis=True):
         search_cuis = [
             [int(ent.label_) for ent in finding_doc.ents]
-            for finding_doc in self._base_snomed_phrase_matcher.pipe(search_terms)
+            for finding_doc in self._snomed_phrase_matcher.pipe(search_terms)
         ]
         search_term_to_cuis = {
             term: cuis for term, cuis in zip(search_terms, search_cuis)
         }
-        custom_phrase_matcher = self._create_custom_phrase_matcher(search_term_to_cuis)
+        cui_to_search_term = defaultdict(set)
+        for search_term, cuis in search_term_to_cuis.items():
+            for cui in cuis:
+                cui_to_search_term[cui].add(search_term)
+                if include_child_cuis:
+                    for child_cui in self._snomed_lookup.get_child_cuis(cui):
+                        cui_to_search_term[child_cui].add(search_term)
+        return cui_to_search_term
 
-        annotated_notes = custom_phrase_matcher.pipe(texts)
-        cui_to_note_idx_and_sent = defaultdict(list)
-        for note_idx, annotated_note in enumerate(annotated_notes):
-            for ent in annotated_note.ents:
-                cui_to_note_idx_and_sent[int(ent.label_)].append((note_idx, ent.sent))
+    @staticmethod
+    def _merge_overlapping_sorted_spans(spans: List[Span]) -> List[Span]:
+        if len(spans) <= 1:
+            return spans
+        merged_spans = []
+        current_span = spans[0]
+        for span in spans[1:]:
+            if span.doc == current_span.doc and span.start <= current_span.end:
+                if span.end >= current_span.end:
+                    current_span.end = span.end
+                else:
+                    # Span is dropped if sub span
+                    pass
+            else:
+                merged_spans.append(current_span)
+                current_span = span
+        merged_spans.append(current_span)
+        return merged_spans
 
-        search_term_to_note_start_idxs_and_sents = {
-            search_term: sorted(
-                note_idx_and_sent
-                for cui in cuis
-                for note_idx_and_sent in cui_to_note_idx_and_sent[cui]
+    @staticmethod
+    def _filter_repeated_span_texts(spans: List[Span]) -> List[Span]:
+        seen_span_texts: Set[str] = set()
+        seen_add = seen_span_texts.add
+        return [
+            span
+            for span in spans
+            if not (span.text in seen_span_texts or seen_add(span.text))
+        ]
+
+    def __call__(
+        self, texts: List[str], search_terms: Set[str]
+    ) -> Dict[str, List[Span]]:
+        cui_to_search_terms = self._get_cui_to_search_terms(search_terms)
+        search_term_to_extract_spans = defaultdict(list)
+        for idx, doc in enumerate(self._snomed_phrase_matcher.pipe(texts)):
+            doc.user_data = {"idx": idx}
+            for ent in doc.ents:
+                for ent_search_term in cui_to_search_terms.get(int(ent.label_), set()):
+                    search_term_to_extract_spans[ent_search_term].append(
+                        Span(
+                            doc,
+                            max(0, ent.start - self._token_window_size),
+                            min(ent.end + self._token_window_size, len(ent.doc)),
+                        )
+                    )
+
+        search_term_to_filtered_extract_spans = {
+            search_term: self._filter_repeated_span_texts(
+                self._merge_overlapping_sorted_spans(extract_spans)
             )
-            for search_term, cuis in search_term_to_cuis.items()
+            for search_term, extract_spans in search_term_to_extract_spans.items()
         }
-        for (
-            search_term,
-            note_idx_and_sents,
-        ) in search_term_to_note_start_idxs_and_sents.items():
-            seen: Set[str] = set()
-            seen_add = seen.add
-            search_term_to_note_start_idxs_and_sents[search_term] = [
-                (note_idx, sent)
-                for note_idx, sent in note_idx_and_sents
-                if not (sent.text in seen or seen_add(sent.text))
-            ]
 
-        return search_term_to_note_start_idxs_and_sents
+        return search_term_to_filtered_extract_spans
