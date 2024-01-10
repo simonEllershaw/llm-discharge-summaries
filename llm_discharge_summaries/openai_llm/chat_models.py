9305ae1cd6f8850a8c0ba0f4a9e3963ec7e6ec29
simonEllershaw <simje@live.co.uk>
2024-01-10 18:33:26 +0000

    Tidy mimic generation notebook

--- llm_discharge_summaries/openai_llm/chat_models.py
+++ llm_discharge_summaries/openai_llm/chat_models.py
@@ -1,18 +1,17 @@
 from abc import abstractmethod
 
 import openai
-from openai.error import TryAgain
 
 from llm_discharge_summaries.openai_llm.message import Message, Role
 
 
 class ChatModel:
     @abstractmethod
-    def query(self, messages: list[Message], num_retries=0) -> Message:
+    def query(self, messages: list[Message]) -> Message:
         pass
 
     @abstractmethod
-    async def aquery(self, messages: list[Message], num_retries=0) -> Message:
+    async def aquery(self, messages: list[Message]) -> Message:
         pass
 
 
@@ -25,7 +24,6 @@ class AzureOpenAIChatModel(ChatModel):
         engine,
         temperature=0,
         timeout=20,
-        max_retries=3,
     ):
         super().__init__()
         openai.api_type = "azure"
@@ -36,40 +34,26 @@ class AzureOpenAIChatModel(ChatModel):
         self.engine = engine
         self.temperature = temperature
         self.timeout = timeout
-        self.max_retries = max_retries
 
-    def query(self, messages: list[Message], num_retries=0, **kwargs) -> Message:
-        try:
-            response = openai.ChatCompletion.create(
-                engine=self.engine,
-                messages=[message.dict() for message in messages],
-                temperature=self.temperature,
-                timeout=self.timeout,
-                **kwargs
-            )
-        except TryAgain as e:
-            if num_retries < self.max_retries:
-                return self.query(messages, num_retries + 1, **kwargs)
-            else:
-                raise e
+    def query(self, messages: list[Message]) -> Message:
+        response = openai.ChatCompletion.create(
+            engine=self.engine,
+            messages=[message.dict() for message in messages],
+            temperature=self.temperature,
+            timeout=self.timeout,
+        )
         return Message(
             role=Role.ASSISTANT,
             content=(response["choices"][0]["message"]["content"]),
         )
 
-    async def aquery(self, messages: list[Message], num_retries=0) -> Message:
-        try:
-            response = await openai.ChatCompletion.acreate(
-                engine=self.engine,
-                messages=[message.dict() for message in messages],
-                temperature=self.temperature,
-                timeout=self.timeout,
-            )
-        except TryAgain as e:
-            if num_retries < self.max_retries:
-                return self.query(messages, num_retries + 1)
-            else:
-                raise e
+    async def aquery(self, messages: list[Message]) -> Message:
+        response = await openai.ChatCompletion.acreate(
+            engine=self.engine,
+            messages=[message.dict() for message in messages],
+            temperature=self.temperature,
+            timeout=self.timeout,
+        )
         return Message(
             role=Role.ASSISTANT,
             content=(response["choices"][0]["message"]["content"]),
